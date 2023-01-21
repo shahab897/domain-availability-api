3e741cd44b2f87ce1da716d7cbcda87907866d82
Muhammad Shahab Rasheed <shahab897@gmail.com>
2023-01-21 22:09:10 +0500

    Added Code to find domain name

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const whois = require('whois');
+
+const app = express();
+
+app.get('/domain/:name', (req, res) => {
+    const domainName = req.params.name;
+
+    whois.lookup(domainName, function (err, data) {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        } else {
+            if (data.indexOf("No match for") != -1) {
+                res.json({ status: 'available' });
+            } else {
+                res.json({ status: 'unavailable' });
+            }
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
