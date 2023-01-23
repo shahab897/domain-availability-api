519b54da80a791402382ec63bc876fb81847034b
Muhammad Shahab Rasheed <shahab897@gmail.com>
2023-01-23 22:27:46 +0500

    Added Post Example For API

--- index.js
+++ index.js
@@ -19,6 +19,23 @@ app.get('/domain/:name', (req, res) => {
     });
 });
 
+/*Post Example*/
+app.post('/domain', (req, res) => {
+    const domainName = req.body.name;
+
+    whois.lookup(domainName, function (err, data) {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        } else {
+            if (data.indexOf("No match for") != -1) {
+                res.json({ status: 'available' });
+            } else {
+                res.json({ status: 'unavailable' });
+            }
+        }
+    });
+});
+
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
